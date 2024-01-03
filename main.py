b61e20b69129b8f3a86a5bfcedebdad878876f98
aerithnetzer <aerithnetzer@outlook.com>
2024-01-03 17:25:22 -0600

    Get csv runnning

---
+++ main.py
@@ -0,0 +1,77 @@
+from habanero import cn
+import requests
+import random
+import pandas as pd
+import csv
+import os
+
+def get_random_doi():
+
+    for i in range(50):
+        # Define the URL for the OpenAlex API
+        url = "https://api.openalex.org/works/random"
+
+        # Make a GET request to the OpenAlex API
+        response = requests.get(url)
+
+        # Parse the JSON response
+        data = dict(response.json())
+
+        if data['doi'] != None and data['language'] == 'en':
+            return data['doi']
+        else:
+            continue
+
+def get_bibtex(doi):
+    try:
+        return cn.content_negotiation(ids = doi, format = 'text', style='bibtex')
+    except requests.exceptions.HTTPError as e:
+        print(f"An HTTP error occurred: {e}")
+
+def get_random_style(doi):
+
+    styles = ['apa', 'harvard3', 'elsevier-harvard', 'mla', 'ecoscience']
+    random_style = random.choice(styles)
+    styled_citation = cn.content_negotiation(ids = doi, format = 'text', style=random_style)
+    return styled_citation, random_style
+
+def main():
+    # Create a dataframe to store the results
+    df = pd.DataFrame(columns=['doi', 'bibtex', 'citation', 'citation_format'])
+
+    # Define the URL for the Crossref API
+    doi = get_random_doi()
+
+    # Get the bibtex
+    bibtex = get_bibtex(doi)
+
+    try:
+        if bibtex is None:
+            return
+        
+        bibtex = bibtex.replace('\n', ' ')
+        # Get the citation
+        citation, random_style = get_random_style(doi)
+
+        # Remove newlines from the citation
+        citation = citation.replace('\n', ' ')
+
+        # Add the results to the dataframe, using concat
+        df = pd.concat([df, pd.DataFrame([[doi, bibtex, citation, random_style]], columns=['doi', 'bibtex', 'citation', 'citation_format'])], ignore_index=True)
+        print(df)
+
+        # Check if the output.csv file already exists
+        if os.path.isfile('output.csv'):
+            # Append the dataframe to the existing file
+            df.to_csv('output.csv', mode='a', header=False, quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+        else:
+            # Save the dataframe to a new file
+            df.to_csv('output.csv', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+
+        # Print the citation
+        print(citation)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+while True:
+    main()
--- manuscript.md
+++ manuscript.md
@@ -2,4 +2,5 @@
 
 ## Abstract
 
-This paper evaluates the effectiveness of large language models in converting plain-text citations to machine-readable formats.
+In this paper we introduce a dataset of plaintext citations in a variety of formats. We then evaluate the performance of a series of large language models on converting these plaintext citations to machine-readable formats. Finally, we discuss the challenges of compiling this database and the necessity of a standardized machine-readable bibliography format.
+
